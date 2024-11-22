142cb0656b63f0b660eccb5904b9f23b99210550
Aradhya Shukla <aradhya1703@gmail.com>
2024-11-22 23:27:01 +0530

    Allowing CORS

--- server.js
+++ server.js
@@ -6,10 +6,15 @@ const { Server } = require("socket.io");
 const ACTIONS = require("./src/Actions");
 
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 
-app.use(express.static("build"));
-app.use((req, res, next) => {
+app.use(express.static(path.join(__dirname, "build")));
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
