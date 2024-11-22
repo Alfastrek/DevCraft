101695575373020bcc8666c02dcac55d1921f58e
Aradhya Shukla <aradhya1703@gmail.com>
2024-11-22 18:02:56 +0530

    Update Editor.js

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -37,6 +37,7 @@ const Editor = ({ socketRef, roomId, onCodeChange }) => {
       });
     }
     init();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
